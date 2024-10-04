89b3ed9ac370f3b5f8936868abf9f3a6eae0d746
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-04 17:58:13 +0100

    feat(docs): Adding docs on how to use the package (#6)
    * Adding docs on how to use the package
    
    * Updated examples
    
    * Updated examples

---
+++ examples/basic/basic.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/Jacobbrewer1/patcher"
+)
+
+type Person struct {
+	ID   *int    `db:"id"`
+	Name *string `db:"name"`
+}
+
+type PersonWhere struct {
+	ID *int `db:"id"`
+}
+
+func NewPersonWhere(id int) *PersonWhere {
+	return &PersonWhere{
+		ID: &id,
+	}
+}
+
+func (p *PersonWhere) Where() (string, []any) {
+	return "id = ?", []any{*p.ID}
+}
+
+func main() {
+	const jsonStr = `{"id": 1, "name": "john"}`
+
+	person := new(Person)
+	if err := json.Unmarshal([]byte(jsonStr), person); err != nil {
+		panic(err)
+	}
+
+	condition := NewPersonWhere(*person.ID)
+
+	sqlStr, args, err := patcher.GenerateSQL(
+		person,
+		patcher.WithTable("people"),
+		patcher.WithWhere(condition),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(sqlStr)
+	fmt.Println(args)
+}
