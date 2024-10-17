37a7ba88185404bbce5094e538ca9a8da4d63817
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-17 12:34:14 +0100

    feat(diffpatcher): returning no changes error when there are no changes (#37)
    returning no changes error when there are no changes

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package patcher
 
-import "reflect"
+import (
+	"errors"
+	"reflect"
+)
 
 // ptr returns a pointer to the value passed in.
 func ptr[T any](v T) *T {
@@ -14,4 +17,16 @@ func isPointerToStruct[T any](t T) bool {
 	}
 
 	return rv.Elem().Kind() == reflect.Struct
+}
+
+// IgnoreNoChanges ignores the ErrNoChanges error. This is useful when you want to ignore the error when no changes
+// were made. Please ensure that you are still handling the errors as needed. We will return a "nil" patch when there
+// are no changes as the ErrNoChanges error is returned.
+func IgnoreNoChanges(err error) error {
+	switch {
+	case errors.Is(err, ErrNoChanges):
+		return nil
+	default:
+		return err
+	}
 }
