72229dd965b4d9c72bf98930672b31cd289ab5fa
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-23 15:49:12 +0100

    adding example for loader opts

---
+++ examples/loader_with_opts/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jacobbrewer1/patcher"
+)
+
+type Something struct {
+	Number             int
+	Text               string
+	PrePopulated       string
+	NewText            string
+	ZeroValue          int
+	NilValue           *string
+	IgnoredField       string
+	IgnoredFieldTwo    string
+	IgnoredFieldByFunc string
+}
+
+func main() {
+	str := "nil value"
+
+	s := Something{
+		Number:             5,
+		Text:               "Hello",
+		PrePopulated:       "PrePopulated",
+		ZeroValue:          7,
+		NilValue:           &str,
+		IgnoredField:       "Ignored",
+		IgnoredFieldTwo:    "Ignored Two",
+		IgnoredFieldByFunc: "Ignored By Func",
+	}
+
+	n := Something{
+		Number:             6,
+		Text:               "Old Text",
+		NewText:            "New Text",
+		ZeroValue:          0,
+		NilValue:           nil,
+		IgnoredField:       "Diff Ignored",
+		IgnoredFieldTwo:    "Diff Ignored Two",
+		IgnoredFieldByFunc: "Diff Ignored By Func",
+	}
+
+	// The patcher.LoadDiff function will apply the changes from n to s.
+	if err := patcher.LoadDiff(&s, &n,
+		patcher.WithIncludeZeroValues(),
+		patcher.WithIncludeNilValues(),
+		patcher.WithIgnoredFields("ignoredField", "IgNoReDfIeLdTwO"),
+		patcher.WithIgnoredFieldsFunc(func(fieldName string, oldValue, newValue interface{}) bool {
+			return fieldName == "ignoredfieldbyfunc"
+		}),
+	); err != nil {
+		panic(err)
+	}
+
+	// Output:
+	// 6
+	// Old Text
+	// PrePopulated
+	// New Text
+	// 0
+	// <nil>
+	// Ignored
+	// Ignored Two
+	// Ignored By Func
+	fmt.Println(s.Number)
+	fmt.Println(s.Text)
+	fmt.Println(s.PrePopulated)
+	fmt.Println(s.NewText)
+	fmt.Println(s.ZeroValue)
+	fmt.Println(s.NilValue)
+	fmt.Println(s.IgnoredField)
+	fmt.Println(s.IgnoredFieldTwo)
+	fmt.Println(s.IgnoredFieldByFunc)
+}
--- loader_opts.go
+++ loader_opts.go
@@ -4,12 +4,18 @@ import "strings"
 
 type LoaderOption func(*loader)
 
+// WithIncludeZeroValues sets whether zero values should be included in the patch.
+//
+// This is useful when you want to set a field to zero.
 func WithIncludeZeroValues() func(*loader) {
 	return func(l *loader) {
 		l.includeZeroValues = true
 	}
 }
 
+// WithIncludeNilValues sets whether nil values should be included in the patch.
+//
+// This is useful when you want to set a field to nil.
 func WithIncludeNilValues() func(*loader) {
 	return func(l *loader) {
 		l.includeNilValues = true
@@ -19,6 +25,9 @@ func WithIncludeNilValues() func(*loader) {
 // WithIgnoredFields sets the fields to ignore when patching.
 //
 // This should be the actual field name, not the JSON tag name or the db tag name.
+//
+// Note. When we parse the slice of strings, we convert them to lowercase to ensure that the comparison is
+// case-insensitive.
 func WithIgnoredFields(fields ...string) func(*loader) {
 	return func(l *loader) {
 		if len(fields) == 0 {
@@ -34,6 +43,9 @@ func WithIgnoredFields(fields ...string) func(*loader) {
 }
 
 // WithIgnoredFieldsFunc sets a function that determines whether a field should be ignored when patching.
+//
+// Note. The field name is wrapped with `strings.ToLower` before being passed to this function, so please ensure that
+// the field name is in lowercase if you are comparing it with this function.
 func WithIgnoredFieldsFunc(f func(fieldName string, oldValue, newValue any) bool) func(*loader) {
 	return func(l *loader) {
 		if f == nil {
