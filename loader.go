acf289cf4d3d5b9539c85aa9107938bcea9fe478
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-10-23 13:50:23 +0100

    adding the ability to configure the laoder

--- loader.go
+++ loader.go
@@ -10,6 +10,40 @@ var (
 	ErrInvalidType = errors.New("invalid type: must pointer to struct")
 )
 
+type loader struct {
+	// includeZeroValues determines whether zero values should be included in the patch
+	includeZeroValues bool
+
+	// includeNilValues determines whether nil values should be included in the patch
+	includeNilValues bool
+
+	// includeEmptyValues determines whether empty values should be included in the patch
+	includeEmptyValues bool
+
+	// ignoreFields is a list of fields to ignore when patching
+	ignoreFields []string
+
+	// ignoreFieldsFunc is a function that determines whether a field should be ignored
+	ignoreFieldsFunc func(string) bool
+}
+
+func newLoader(opts ...loaderOption) *loader {
+	// Default options
+	l := &loader{
+		includeZeroValues:  false,
+		includeNilValues:   false,
+		includeEmptyValues: false,
+		ignoreFields:       nil,
+		ignoreFieldsFunc:   nil,
+	}
+
+	for _, opt := range opts {
+		opt(l)
+	}
+
+	return l
+}
+
 // LoadDiff inserts the fields provided in the new struct pointer into the old struct pointer and injects the new
 // values into the old struct.
 //
@@ -17,11 +51,11 @@ var (
 //
 // This can be if you are inserting a patch into an existing object but require a new object to be returned with
 // all fields.
-func LoadDiff[T any](old *T, newT *T) error {
-	return loadDiff(old, newT)
+func LoadDiff[T any](old *T, newT *T, opts ...loaderOption) error {
+	return newLoader(opts...).loadDiff(old, newT)
 }
 
-func loadDiff[T any](old T, newT T) error {
+func (l *loader) loadDiff(old, newT any) error {
 	if !isPointerToStruct(old) || !isPointerToStruct(newT) {
 		return ErrInvalidType
 	}
@@ -40,7 +74,7 @@ func loadDiff[T any](old T, newT T) error {
 			// If the embedded field is a pointer, dereference it
 			if oElem.Field(i).Kind() == reflect.Ptr {
 				if !oElem.Field(i).IsNil() && !nElem.Field(i).IsNil() {
-					if err := loadDiff(oElem.Field(i).Interface(), nElem.Field(i).Interface()); err != nil {
+					if err := l.loadDiff(oElem.Field(i).Interface(), nElem.Field(i).Interface()); err != nil {
 						return err
 					}
 				} else if nElem.Field(i).IsValid() && !nElem.Field(i).IsNil() {
@@ -50,7 +84,7 @@ func loadDiff[T any](old T, newT T) error {
 				continue
 			}
 
-			if err := loadDiff(oElem.Field(i).Addr().Interface(), nElem.Field(i).Addr().Interface()); err != nil {
+			if err := l.loadDiff(oElem.Field(i).Addr().Interface(), nElem.Field(i).Addr().Interface()); err != nil {
 				return err
 			}
 			continue
@@ -58,7 +92,7 @@ func loadDiff[T any](old T, newT T) error {
 
 		// If the field is a struct, we need to recursively call LoadDiff
 		if oElem.Field(i).Kind() == reflect.Struct {
-			if err := loadDiff(oElem.Field(i).Addr().Interface(), nElem.Field(i).Addr().Interface()); err != nil {
+			if err := l.loadDiff(oElem.Field(i).Addr().Interface(), nElem.Field(i).Addr().Interface()); err != nil {
 				return err
 			}
 			continue
---
+++ loader_opts.go
@@ -0,0 +1,45 @@
+package patcher
+
+type loaderOption func(*loader)
+
+func WithIncludeZeroValues() func(*loader) {
+	return func(l *loader) {
+		l.includeZeroValues = true
+	}
+}
+
+func WithIncludeNilValues() func(*loader) {
+	return func(l *loader) {
+		l.includeNilValues = true
+	}
+}
+
+func WithIncludeEmptyValues() func(*loader) {
+	return func(l *loader) {
+		l.includeEmptyValues = true
+	}
+}
+
+// WithIgnoredFields sets the fields to ignore when patching.
+//
+// This should be the actual field name, not the JSON tag name or the db tag name.
+func WithIgnoredFields(fields ...string) func(*loader) {
+	return func(l *loader) {
+		if len(fields) == 0 {
+			return
+		}
+
+		l.ignoreFields = fields
+	}
+}
+
+// WithIgnoredFieldsFunc sets a function that determines whether a field should be ignored when patching.
+func WithIgnoredFieldsFunc(f func(string) bool) func(*loader) {
+	return func(l *loader) {
+		if f == nil {
+			return
+		}
+
+		l.ignoreFieldsFunc = f
+	}
+}
